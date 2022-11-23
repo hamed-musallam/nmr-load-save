f10ad59b731f29f4966be9d61a30e72daa8f7584
hamed-musallam <hamed.musallam@gmail.com>
2022-11-23 11:14:33 +0100

    feat: skip 2d fid spectra

--- src/migration/migrateToVersion4.ts
+++ src/migration/migrateToVersion4.ts
@@ -1,9 +1,9 @@
 /*  NMRium Version: 0.33.0
     Notes: Any 2d file containing fid spectra should be regenerated with nmr-load-save package because the parser did not handle it correctly before this version
 
-    * changes from version 3 to version 4:
-       change the 2d data structure, in version 3 the 2D FT spectra have this structure which represents the real part that should be migrated to the new structure for 2D
-        FT which contains `rr`, `ii`, `ri` and `ir` where `r` stands for the real part and `i` stands for the imaginary part
+    *changes from version 3 to version 4:
+      1- change the 2d data structure, in version 3 the 2D FT spectra have this structure which represents the real part that should be migrated to the new structure for 2D
+         FT which contains `rr`, `ii`, `ri` and `ir` where `r` stands for the real part and `i` stands for the imaginary part
             Data
                 {
                     data: {
@@ -34,22 +34,31 @@
                             }
                 }
 
-
             paths:
               - spectra > data
+
+      2- skip the 2D FID spectra.
+
     */
 
 export default function migrateToVersion4(data: any): any {
   if (data?.version === 4) return data;
 
-  const newData = { ...data, version: 4 };
+  const spectra = [];
 
-  for (const spectrum of newData.spectra) {
+  for (const spectrum of data.spectra) {
     const { dimension, isFt } = spectrum.info;
-    if (dimension === 2 && isFt) {
-      spectrum.data = { rr: spectrum.data };
+    if (dimension === 2) {
+      if (isFt) {
+        spectrum.data = { rr: spectrum.data };
+      } else {
+        // skip pushing 2d fid spectrum to spectra array
+        continue;
+      }
     }
+
+    spectra.push(spectrum);
   }
 
-  return newData;
+  return { ...data, spectra, version: 4 };
 }
